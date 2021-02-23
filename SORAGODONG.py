e9ee96942363e1214b2f321299e3ed5255103e7f
PAPE-FELIX <58857012+PAPE-FELIX@users.noreply.github.com>
2021-02-23 14:56:43 +0900

    Update SORAGODONG.py

--- SORAGODONG.py
+++ SORAGODONG.py
@@ -2,61 +2,65 @@ import discord
 import random
 import os
 
-soragodong = ['a', 'b', 'c', 'd', 'e']
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print(client.user.id)
-    print("Ready!")
-    game = discord.Game("D.R.I.N.K")
-    await client.change_presence(status=discord.Status.online, activity=game)
+    print('준비 됨.')
 
-@client.event
-async def on_message(message):
-    if message.content.startswith("잘남"):
-        await message.channel.send("가만")
+class MyClient(discord.Client):
 
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
 
-    if message.content.startswith("소라"):
-        randomNum = random.randrange(1, 6)  # 1~6까지 랜덤수
-        print(randomNum)
-        if randomNum == 1:
-            await message.channel.send(embed=discord.Embed(title="안 돼.", color=discord.Color.red()))
-        if randomNum == 2:
-            await message.channel.send(embed=discord.Embed(title="언젠가는.", color=discord.Color.gold()))
-        if randomNum == 3:
-            await message.channel.send(embed=discord.Embed(title="굶어.", color=discord.Color.red()))
-        if randomNum == 4:
-            await message.channel.send(embed=discord.Embed(title="그럼.", color=discord.Color.blue()))
-        if randomNum == 5:
-            await message.channel.send(embed=discord.Embed(title="다시 한 번 물어봐.", color=discord.Color.gold()))
-        if randomNum == 6:
-            await message.channel.send(embed=discord.Embed(title="그럼.", color=discord.Color.blue()))
+        if message.content.startswith('ㅗ'):
+            await message.reply('ㅗ', mention_author=True)
+        if message.content.startswith('개새끼'):
+            await message.reply('뭐 이새끼야', mention_author=True)
 
 
 
 
+        if message.content.startswith('소라'):
 
-    if message.content.startswith("마법"):
-        randomNum = random.randrange(1, 6)  # 1~6까지 랜덤수
-        print(randomNum)
-        if randomNum == 1:
-            await message.channel.send(embed=discord.Embed(title="안 돼.", color=discord.Color.red()))
-        if randomNum == 2:
-            await message.channel.send(embed=discord.Embed(title="언젠가는.", color=discord.Color.gold()))
-        if randomNum == 3:
-            await message.channel.send(embed=discord.Embed(title="굶어.", color=discord.Color.red()))
-        if randomNum == 4:
-            await message.channel.send(embed=discord.Embed(title="그럼.", color=discord.Color.blue()))
-        if randomNum == 5:
-            await message.channel.send(embed=discord.Embed(title="다시 한 번 물어봐.", color=discord.Color.gold()))
-        if randomNum == 6:
-            await message.channel.send(embed=discord.Embed(title="그럼.", color=discord.Color.blue()))
-
-
+            randomNum = random.randrange(1, 3)  # 1~6까지 랜덤수
+            print(randomNum)
+            if randomNum == 1:
+                await message.reply('안돼', mention_author=True)
+            if randomNum == 2:
+                await message.reply('그래', mention_author=True)
+            if randomNum == 3:
+                await message.reply('굶어', mention_author=True)
+            if randomNum == 3:
+                await message.reply('응', mention_author=True)
+            if randomNum == 3:
+                await message.reply('기다려', mention_author=True)
+            if randomNum == 3:
+                await message.reply('다시 한번 물어봐', mention_author=True)
 
 
 
+        if message.content.startswith('마법'):
+
+            randomNum = random.randrange(1, 3)
+            print(randomNum)
+            if randomNum == 1:
+                await message.reply('안돼', mention_author=True)
+            if randomNum == 2:
+                await message.reply('그럼', mention_author=True)
+            if randomNum == 3:
+                await message.reply('굶어', mention_author=True)
+            if randomNum == 3:
+                await message.reply('어, 그래', mention_author=True)
+            if randomNum == 3:
+                await message.reply('기다려', mention_author=True)
+            if randomNum == 3:
+                await message.reply('다시 한번 물어봐', mention_author=True)
+
+client = MyClient()
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
